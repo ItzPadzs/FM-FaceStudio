68618b469c0583c0aa2adc82a717aa85fd2a13a0
ItzPadzs <155378118+ItzPadzs@users.noreply.github.com>
2026-07-26 17:55:24 +0100

    Delete FM-FaceStudio directory

--- FM-FaceStudio/RUN.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist ".venv\Scripts\pythonw.exe" (
-    echo Run INSTALL_AND_OPEN.bat first.
-    pause
-    exit /b 1
-)
-start "" ".venv\Scripts\pythonw.exe" launcher.py
